1c19bd5262ce5a978e217493b206f54ee942289d
Nolife159159 <mants.mockevicius@gmail>
2021-01-27 21:47:33 +0200

    DB

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,16 +13,44 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('zaidejai', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
-            $table->text('profile_photo_path')->nullable();
-            $table->timestamps();
+            $table->string('vardas', 24);
+            $table->string('slaptazodis', 40);
+            $table->string('email', 40)->unique();
+            $table->integer('skin')->nullable();
+            $table->integer('pinigai')->nullable();
+            $table->integer('patirtis')->nullable();
+            $table->integer('pinigai')->nullable();
+            $table->integer('gender')->nullable();
+            $table->dateTime('regdate')->nullable();
+            $table->integer('vip')->nullable();
+            $table->integer('admin')->nullable();
+            $table->integer('mutelaikas')->nullable();
+            $table->integer('VipLaikas')->nullable();
+            $table->integer('AdminLaikas')->nullable();
+            $table->integer('banned')->nullable();
+            $table->integer('BanLaikas')->nullable();
+            $table->integer('darbas')->nullable();
+            $table->integer('uniform')->nullable();
+            $table->integer('wUniform')->nullable();
+            $table->integer('alga')->nullable();
+            $table->integer('Prazaide')->nullable();
+            $table->integer('direktorius')->nullable();
+            $table->integer('pavaduotojas')->nullable();
+            $table->integer('Gaudomumas')->nullable();
+            $table->integer('Rangas')->nullable();
+            $table->integer('bank_pin')->nullable();
+            $table->integer('VisasDarboXP')->nullable();
+            $table->integer('rep')->nullable();
+            $table->string('SlaptasKlausimas',40)->nullable();
+            $table->string('KlausimoAtsakymas',40)->nullable();
+            $table->integer('visp')->nullable();
+            $table->integer('aisp')->nullable();
+            $table->integer('disp')->nullable();
+            $table->integer('totalbans')->nullable();
+            $table->string('adminmutename', 24)->nullable();
+            $table->integer('priziuretojas')->nullable();
         });
     }
 
@@ -33,6 +61,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('zaidejai');
     }
 }
